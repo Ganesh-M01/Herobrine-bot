ab4e6202d03c725f33ccde58eadd687aeeba74d1
Ganesh-M01 <124779430+Ganesh-M01@users.noreply.github.com>
2025-08-15 21:06:22 +0530

    Render Deployment issue fixed

--- ip.py
+++ ip.py
@@ -1,18 +1,21 @@
 from discord.ext import commands
 from discord import app_commands, Interaction, Embed, File, Color
 
-# ---- EDIT THESE THREE VALUES ----
+# ---- EDIT THESE VALUES ----
 JAVA_ADDRESS = "paid.taitcloud.xyz:25575"
 BEDROCK_IP = "paid.taitcloud.xyz"
 BEDROCK_PORT = 25575
 BANNER_PATH = "./assets/banner.gif"
-# ---------------------------------
+ADMIN_ROLE_ID = 1335986334673932378
+MOD_ROLE_ID = 1335986334673932378
+# ---------------------------
 
 class IP(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name="ip", description="Get the server IP and port for Java & Bedrock")
+    @app_commands.checks.has_any_role(ADMIN_ROLE_ID, MOD_ROLE_ID)
     async def ip(self, interaction: Interaction):
         embed = Embed(
             title="🌐 Here is the IP and Port",
@@ -25,11 +28,9 @@ class IP(commands.Cog):
             inline=False
         )
 
-        # Attach banner.gif to embed
         file = File(BANNER_PATH, filename="banner.gif")
         embed.set_image(url="attachment://banner.gif")
 
-        # Add mentions in the message content
         content = "<@&GAMER_ROLE_ID> <@&MINECRAFT_ROLE_ID>"
 
         await interaction.response.send_message(content=content, embed=embed, file=file)
